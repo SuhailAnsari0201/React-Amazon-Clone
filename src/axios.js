803b25bc46b5b4e8ad12de2f6280994f6aec1d44
Suhail Ansari <SuhailAnsari0201@outlook.com>
2020-09-29 13:58:50 +0530

    change backend firebase to heroku

--- src/axios.js
+++ src/axios.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5001/react--clone-84dd2/us-central1/api",
+  baseURL: "http://localhost:5000",
 });
 
 export default instance;
-
-//"https://us-central1-react--clone-84dd2.cloudfunctions.net/api" THE API (cloud function) URL
-//http://localhost:5001/react--clone-84dd2/us-central1/api (localhost url using firebase emulators:start command )
+//"https://react-ama-clone.herokuapp.com/" (heroku live server)
+//http://localhost:5000 (localhost url )
